c5f4487b7e01de556d3bf34cac7c4f810178d464
avearmin <arminradoncic1@gmail.com>
2024-08-28 13:20:35 -0400

    Add 'v1' to api path

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,36 +1,12 @@
 package server
 
 import (
-	"errors"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/avearmin/wisdomwell/internal/api"
-	"github.com/joho/godotenv"
 )
-
-type config struct {
-	Port string
-}
-
-func newConfig() (config, error) {
-	if err := godotenv.Load(); err != nil {
-		return config{}, errors.New("cannot load .env file")
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		return config{}, errors.New("PORT has not been specified")
-	}
-
-	c := config{
-		Port: port,
-	}
-
-	return c, nil
-}
 
 func Start() {
 	config, err := newConfig()
@@ -40,18 +16,18 @@ func Start() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /api/healthz", api.HandlerHealthz)
+	mux.HandleFunc("GET /api/v1/healthz", api.HandlerHealthz)
 
 	corsMux := middlewareCors(mux)
 
 	srv := http.Server{
-		Addr:         ":" + config.Port,
+		Addr:         ":" + config.port,
 		Handler:      corsMux,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 
-	log.Println("Serving on port: " + config.Port)
+	log.Println("Serving on port: " + config.port)
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
