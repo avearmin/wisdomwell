eafd509e2d9ce7803726d06cb39ec714fc1f7a1d
avearmin <arminradoncic1@gmail.com>
2024-09-14 10:17:07 -0400

    Add Get Post Delete queries for quote_tags

---
+++ sql/queries/quote_tags.sql
@@ -0,0 +1,11 @@
+-- name: GetQuoteTag :one
+SELECT * FROM quote_tags 
+WHERE Quote_ID = $1 AND Tag_ID = $2;
+
+-- name: PostQuoteTag :one
+INSERT INTO quote_tags (Quote_ID, Tag_ID)
+VALUES ($1, $2) RETURNING *;
+
+-- name: DeleteQuoteTag :exec
+DELETE FROM quote_tags
+WHERE Quote_ID = $1 AND Tag_ID = $2;
